bab5edb9ac0ba4afb416340cd3b5ea3d6327ecd8
Matthew Kuntz <matt@mfkuntz.com>
2022-12-05 16:58:23 -0500

    fmt on save

--- lib/days/three/day_three.ex
+++ lib/days/three/day_three.ex
@@ -21,39 +21,44 @@ defmodule Advent.DayThree do
   end
 
   defp part_1(lines) do
-    total = lines
-    |> Enum.map(fn it ->
-      len = String.length(it)
-      middle = trunc(len/2)
+    total =
+      lines
+      |> Enum.map(fn it ->
+        len = String.length(it)
+        middle = trunc(len / 2)
 
-      [a, b] = String.codepoints(it)
-      |> Enum.chunk_every(middle)
+        [a, b] =
+          String.codepoints(it)
+          |> Enum.chunk_every(middle)
 
-      v = MapSet.intersection(MapSet.new(a), MapSet.new(b)) |> MapSet.to_list()
+        v = MapSet.intersection(new_map(a), new_map(b)) |> MapSet.to_list()
 
-      Enum.find_index(@alphabet, fn a -> a == hd(v) end)
-    end)
-    |> Enum.sum()
+        Enum.find_index(@alphabet, fn a -> a == hd(v) end)
+      end)
+      |> Enum.sum()
 
     Logger.debug("part 1: " <> inspect(total))
   end
 
   defp part_2(lines) do
-    total = lines
-    |> Enum.chunk_every(3)
-    |> Enum.map(fn it ->
-      [a, b, c] = it
+    total =
+      lines
+      |> Enum.chunk_every(3)
+      |> Enum.map(fn it ->
+        [a, b, c] = it
 
-      v = MapSet.intersection(MapSet.new(String.codepoints(a)), MapSet.new(String.codepoints(b)))
-      |> MapSet.intersection(MapSet.new(String.codepoints(c)))
-      |> MapSet.to_list()
+        v =
+          MapSet.intersection(new_map(a), new_map(b))
+          |> MapSet.intersection(new_map(c))
+          |> MapSet.to_list()
 
-      Enum.find_index(@alphabet, fn a -> a == hd(v) end)
-    end)
-    |> Enum.sum()
+        Enum.find_index(@alphabet, fn a -> a == hd(v) end)
+      end)
+      |> Enum.sum()
 
     Logger.debug("part 2: " <> inspect(total))
   end
 
-
+  defp new_map(lines) when is_binary(lines), do: new_map(String.codepoints(lines))
+  defp new_map(lines), do: MapSet.new(lines)
 end
